aa0ab0bd70b41fc7cdcbdfe70383f0a4459e9e4f
Georges <georgesgil22@gmail.com>
2023-11-05 12:52:39 -0400

    Prueba Seria #8

--- main.tf
+++ main.tf
@@ -29,35 +29,94 @@ resource "digitalocean_droplet" "web2" {
     timeout     = "2m"
   }
 
-  provisioner "remote-exec" {
+
+    provisioner "remote-exec" {
     inline = [
-    "sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 35696F43FC7DB4C2",
-    "sudo killall apt apt-get",
-    "sudo ufw allow 1339",
-    "sudo ufw allow 443",
-    "sudo ufw allow 5432",
-    "sudo ufw allow 5433",
-    "sudo ufw allow 8000",
-    "sudo rm /var/lib/apt/lists/lock",
-    "sudo rm /var/cache/apt/archives/lock",
-    "sudo rm /var/lib/dpkg/lock*",
-    "sudo dpkg --configure -a",
     "sudo apt-get update",
     "sudo apt install -y git",
     "git clone https://github.com/GeorgesGil/app-semana-05.git",
     "cd app-semana-05/db-app",
+    "curl -L https://github.com/docker/compose/releases/download/v2.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
+    "chmod +x /usr/local/bin/docker-compose",
     "docker-compose --version",
     "docker network create mynetwork",
     "docker-compose up -d --build",
     "docker container ls",
     "cd ../nginx-app",
+    "curl -L https://github.com/docker/compose/releases/download/v2.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
+    "chmod +x /usr/local/bin/docker-compose",
     "docker-compose --version",
     "docker-compose up -d --build",
     "docker container ls",
     "sudo apt-get install -y postgresql-client",
-    
+    "sudo ufw allow 1339",
+    "sudo ufw allow 443",
+    "sudo ufw allow 5432",
+    "sudo ufw allow 5433",
+    "sudo ufw allow 8000"
   ]
 }
 
 
 }
+
+
+
+
+
+
+# provisioner "remote-exec" {
+#     inline = [
+#     "sudo apt-get update",
+#     "sudo apt install -y git",
+#     "git clone https://github.com/GeorgesGil/AlgoritmosParalelos.git",
+#     "cd AlgoritmosParalelos/Semana-04/go-docker-app/db-app",
+#     "curl -L https://github.com/docker/compose/releases/download/v2.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
+#     "chmod +x /usr/local/bin/docker-compose",
+#     "docker-compose --version",
+#     "docker network create mynetwork",
+#     "docker-compose up -d --build",
+#     "docker container ls",
+#     "cd ../nginx-app",
+#     "curl -L https://github.com/docker/compose/releases/download/v2.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
+#     "chmod +x /usr/local/bin/docker-compose",
+#     "docker-compose --version",
+#     "docker-compose up -d --build",
+#     "docker container ls",
+#     "sudo apt-get install -y postgresql-client",
+#     "sudo ufw allow 1339",
+#     "sudo ufw allow 443",
+#     "sudo ufw allow 5432",
+#     "sudo ufw allow 5433",
+#     "sudo ufw allow 8000"
+#   ]
+# }
+#   provisioner "remote-exec" {
+#     inline = [
+#     "sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 35696F43FC7DB4C2",
+#     "sudo killall apt apt-get",
+#     "sudo ufw allow 1339",
+#     "sudo ufw allow 443",
+#     "sudo ufw allow 5432",
+#     "sudo ufw allow 5433",
+#     "sudo ufw allow 8000",
+#     "sudo rm /var/lib/apt/lists/lock",
+#     "sudo rm /var/cache/apt/archives/lock",
+#     "sudo rm /var/lib/dpkg/lock*",
+#     "sudo dpkg --configure -a",
+#     "sudo apt-get update",
+#     "sudo apt install -y git",
+#     "git clone https://github.com/GeorgesGil/app-semana-05.git",
+#     "cd app-semana-05/db-app",
+#     "docker-compose --version",
+#     "docker network create mynetwork",
+#     "docker-compose up -d --build",
+#     "docker container ls",
+#     "cd ../nginx-app",
+#     "docker-compose --version",
+#     "docker-compose up -d --build",
+#     "docker container ls",
+#     "sudo apt-get install -y postgresql-client",
+
+#   ]
+# }
